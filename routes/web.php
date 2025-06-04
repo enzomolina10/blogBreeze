bd71699ebebbf6b959cdedbd8ffaac41d63ac2e8
enzo molina <enzomolina2002@gmail.com>
2025-06-04 10:18:47 -0300

    Post en models

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 // ruta de TP3-LARAVEL publicas
